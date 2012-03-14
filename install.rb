02298efd20a57a2541baead2c91a8b6f2f406aff
David Parry <david.parry@suranyami.com>
2012-03-15 10:15:01 +1100

    Removed dependency on trollop

--- install.rb
+++ install.rb
@@ -1,32 +1,20 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'trollop'
-
-opts = Trollop::options do
-  banner "Install dot files into your home directory"
-  opt :individual, "filename", :type => :string, :required => false, :short => "i"
-  opt :force, :type => :boolean, :default => :false, :short => "f"
-end
 
 home = File.expand_path('~')
 
 files = Dir['*'].to_a
-files = opts[:individual] if opts[:individual]
 files = [files] unless files.is_a? Array
 
 files.each do |file|
   next if file =~ /install|osx|Gemfile*/
   target = File.join(home, ".#{file}")
 
-  ok_to_delete = if opts[:force]
-    true
-  else
-    if File.exists?(target)
-      print "Overwrite file '#{target}'? (y/n) "
-      response = gets.chomp.downcase
-      response == 'y'
-    end
+  ok_to_delete = if File.exists?(target)
+    print "Overwrite file '#{target}'? (y/n) "
+    response = gets.chomp.downcase
+    response == 'y'
   end
 
   if File.exists?(target) 
